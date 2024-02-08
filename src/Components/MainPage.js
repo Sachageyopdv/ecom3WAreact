26a00b21c0a093714b1f6842c0dea8feaf687744
Sachageyo <s.nguyenvanbau@gmail.com>
2024-02-08 12:53:03 +0100

    Added simple card to display products

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -4,20 +4,33 @@ import styled from "styled-components";
 import Product from "./Product";
 
 const MyDiv = styled.div`
-  height: 100vh;
-  width: 100vw;
-  flex: flex-wrap;
+  height: auto;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
 `;
 
 const MainPage = () => {
   axios(
-    "https://backend-exam-react-w2-g9us7xub4-nissim-shoprahacom.vercel.app/api/product"
+    "https://api-3wa-ecomm-524fde41edfa.herokuapp.com/api/product"
   )
     .then((data) => console.log(data))
     .catch((error) => console.error("Error fetching data:", error));
 
   return (
     <MyDiv>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
       <Product></Product>
     </MyDiv>
   );
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,16 +1,39 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import CG from "../img/img.jpg";
 
 const Card = styled.div`
   border: grey solid 1px;
   background-color: grey;
-  border-radius: 50px;
-  max-width: 15%;
-  height: 500px: 
+  border-radius: 10px;
+  width: 19%;
+  height: 300px;
+  margin: 5px;
+  display: flex;
+  flex-direction: column;
+  column-count: 5;
+`;
+
+const CardImage = styled.img`
+  height: 200px;
+  width: 200px;
+  padding: 10px;
+  align-self
 `;
 
 const Product = () => {
-  return <Card>BONJOUR</Card>;
+  return (
+    <>
+      <Card>
+        <Link to="">
+          <CardImage src={CG}></CardImage>
+          <p>Nom du produit</p>
+        </Link>
+        <button>Prix</button>
+      </Card>
+    </>
+  );
 };
 
 export default Product;
