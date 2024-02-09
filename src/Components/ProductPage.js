fb7e1b64a0966613b947fc3dde56ce0477e6c851
Sachageyo <s.nguyenvanbau@gmail.com>
2024-02-09 10:01:45 +0100

    Basic display of product added to ProductPage

--- src/Components/ProductPage.js
+++ src/Components/ProductPage.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { faker } from "@faker-js/faker";
+import styled from "styled-components";
+
+const Price = styled.p`
+  font-size: 40px;
+`;
 
 const ProductPage = () => {
   let { id } = useParams();
   id = id.substring(1);
-  const [productData, setProductData] = useState({});
-
-  console.log(productData);
+  const [productData, setProductData] = useState();
 
   useEffect(() => {
     axios(`https://api-3wa-ecomm-524fde41edfa.herokuapp.com/api/product/${id}`)
@@ -15,9 +19,21 @@ const ProductPage = () => {
         setProductData(data);
       })
       .catch((error) => console.error("Error fetching data:", error));
-  }, []);
+  }, [id]);
 
-  return <div>ProductPage</div>;
+  return (
+    <>
+      {productData && (
+        <div>
+          <img src={faker.image.url()}></img>
+          <h1>{productData.name}</h1>
+          <label>{productData.color}</label>
+          <Price>{productData.price}€</Price>
+          <p>{productData.desc}</p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ProductPage;
